7eb30bbf9c14e97ae03bedffeaa64d43225f8593
prakhar <prakharjain1114@gmail.com>
2021-02-06 09:22:48 +0530

    initial commit

---
+++ policy.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+torch.manual_seed(0) # set random seed
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Policy(nn.Module):
+    def __init__(self, s_size=6, h_size=32, a_size=3):
+        super(Policy, self).__init__()
+        self.fc1 = nn.Linear(s_size, h_size)
+        self.fc2 = nn.Linear(h_size, a_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1).to(device)
+    
+    def act(self, state):
+#         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        probs = self.forward(state).cuda()
+        m = Categorical(probs)
+        action = m.sample()
+        return action.item() 
+    
+    def get_action_values(self, states):
+        x = F.relu(self.fc1(states))
+        x = self.fc2(x).to(device)
+        return x
+        
